c7e03a3829d939c1fcf9af66848a6707e69161d1
Everton <everton_tracz@hotmail.com>
2026-05-15 21:25:17 -0300

    Executando script para verificação de nota do aluno

--- Exercicios-Aquecimentos/exercicios.py
+++ Exercicios-Aquecimentos/exercicios.py
@@ -1,11 +1,11 @@
-x = int(input("Digite o primeiro número:"))
-y = int(input("Digite o segundo Número:"))
+# x = int(input("Digite o primeiro número:"))
+# y = int(input("Digite o segundo Número:"))
 
-print("O Resultado da sua soma e:", x + y)
-print("O Resultado da sua subtração e:", x - y)
-print("O Resultado da sua multiplicação e:", x * y)
-print("O Resultado da sua divisão e:", x / y)
-print("O Resultado da sua exponenciação e:", x ** y)
+# print("O Resultado da sua soma e:", x + y)
+# print("O Resultado da sua subtração e:", x - y)
+# print("O Resultado da sua multiplicação e:", x * y)
+# print("O Resultado da sua divisão e:", x / y)
+# print("O Resultado da sua exponenciação e:", x ** y)
 
 # import numpy as np
 
@@ -18,15 +18,15 @@ print("O Resultado da sua exponenciação e:", x ** y)
 
 # media = np.mean(x)
 
-# print(f'A sua Lista é: {x} E a media da Lista é: {media} E o tamanho da lista é: {len(x)}')
+print(f'A sua Lista é: {x} E a media da Lista é: {media} E o tamanho da lista é: {len(x)}')
 
-# nota = float(input("Digite a nota do aluno:"))
+nota = float(input("Digite a nota do aluno:"))
 
-# if nota > 7:
-#     print("Aluno Aprovado")
+if nota > 7:
+    print("Aluno Aprovado")
 
-# elif nota < 5:
-#     print("Aluno Reprovado")
+elif nota < 5:
+    print("Aluno Reprovado")
 
-# else:
-#     print("Aluno em Recuperação")
+else:
+    print("Aluno em Recuperação")
